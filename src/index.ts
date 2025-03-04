422f912c96ec201d82b057b2844cae2811427de6
George <george.soares509@gmail.com>
2025-03-04 15:53:14 -0300

    Remove dynamo connection

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,18 @@
-import express, { Request, Response } from 'express';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, PutCommand, GetCommand } from '@aws-sdk/lib-dynamodb';
-import dotenv from 'dotenv';
+import express from 'express'
 
-dotenv.config();
+import { Router, Request, Response } from 'express';
 
 const app = express();
-app.use(express.json());
 
-const TABLE_NAME = 'general';
+const route = Router()
 
-const client = new DynamoDBClient({
-  region: process.env.AWS_REGION!,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
+app.use(express.json())
 
-const dynamoDB = DynamoDBDocumentClient.from(client);
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello world with Typescript v2' })
+})
 
-app.post('/insert', async (req: Request, res: Response): Promise<any> => {
-  const { key, value } = req.body;
+app.use(route)
 
-  if (!key || !value) {
-    return res.status(400).json({ error: 'Os campos key e value são obrigatórios.' });
-  }
-
-  const params = new PutCommand({
-    TableName: TABLE_NAME,
-    Item: {
-      Key: key,
-      Value: value,
-    },
-  });
-
-  try {
-    await dynamoDB.send(params);
-    return res.json({ message: 'Item inserido com sucesso!', item: { Key: key, Value: value } });
-  } catch (error) {
-    console.error('Erro ao inserir item:', error);
-    return res.status(500).json({ error: 'Erro ao inserir item no DynamoDB' });
-  }
-});
-
-app.get('/get/:key', async (req: Request, res: Response): Promise<any> => {
-  const { key } = req.params;
-
-  const params = new GetCommand({
-    TableName: TABLE_NAME,
-    Key: {
-      Key: key,
-    },
-  });
-
-  try {
-    const result = await dynamoDB.send(params);
-
-    if (!result.Item) {
-      return res.status(404).json({ error: 'Item não encontrado' });
-    }
-
-    return res.json({ item: result.Item });
-  } catch (error) {
-    console.error('Erro ao buscar item:', error);
-    return res.status(500).json({ error: 'Erro ao buscar item no DynamoDB' });
-  }
-});
 
 app.listen(3333, () => console.log('Server running on port 3333'));
